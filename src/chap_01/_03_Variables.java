4a220a8d5c0a5d6cbcae58eb883b6bc1f3548001
hong2HSH <tnghd1717@gmail.com>
2023-06-03 22:47:28 +0900

    230603

---
+++ src/chap_01/_03_Variables.java
@@ -0,0 +1,28 @@
+package chap_01;
+
+public class _03_Variables {
+    public static void main(String[] args) {
+        String name;
+        name = "황수홍";
+        int hour = 15;
+
+        System.out.println(name+"님, 배송이 시작됩니다."+hour+"시에 방문 예정입니다.");
+        System.out.println(name+"님, 배송이 완료되었습니다.");
+
+        double score = 90.5;
+        char grade = 'A';
+        name = "수홍";
+        System.out.println(name+"님의 평균 점수는"+score+"점입니다.");
+        System.out.println("학점은"+grade+"입니다.");
+
+        boolean pass = true;
+        System.out.println("이번 시험에 합격했을까요?"+pass);
+
+        double d = 3.141516516;
+        float f = 3.141545646f;
+        System.out.println(d);
+        System.out.println(f);
+
+        i
+    }
+}
